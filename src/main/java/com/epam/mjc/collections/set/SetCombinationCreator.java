7b89db85518ce83a4f982332bcde87801ab18b14
Bakhtiyor22 <baxtiyorabduraximov2203@gmail.com>
2024-12-25 18:12:19 +0500

    commit

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -6,8 +6,10 @@ import java.util.Set;
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
         Set<String> setCombination = new HashSet<>(firstSet);
-        setCombination.addAll(secondSet);
-        setCombination.addAll(thirdSet);
+        setCombination.retainAll(secondSet); // Keep only elements that are in both firstSet and secondSet
+        thirdSet.removeAll(firstSet);
+        thirdSet.removeAll(secondSet);
+        setCombination.addAll(thirdSet); // Add elements from thirdSet that are not in firstSet or secondSet
         return setCombination;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,20 +1,16 @@
+
 package com.epam.mjc.collections.set;
 
 import java.util.List;
-import java.util.NavigableSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
-        TreeSet<Integer> squareSet = new TreeSet<>();
-
-        for (Integer x : sourceList) {
-            squareSet.add(x * x);
-        }
-
-        NavigableSet<Integer> subsetOfSquares = squareSet.subSet(lowerBound, true, upperBound, true);
-
-        return subsetOfSquares;
+        return sourceList.stream()
+                .map(x -> x * x)
+                .filter(x -> x >= lowerBound && x <= upperBound)
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 }
